bdd56d2c1356511cf6207d1765983a0c46a929ce
ishulman <39791045+ishulman@users.noreply.github.com>
2021-10-16 14:30:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import requests
 import datadog
 from datadog import initialize, statsd
 
-api_url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/all-access/all-agents/Datadog/monthly/20200101/20210101"
-hdr = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(api_url, headers=hdr)
-data = response.json()
+from datetime import date
 
-print(data['items'][0]['views'])
+today = date.today().strftime('%Y%m%d')
 
-last_month_views = data['items'][0]['views']
+languages = ["en", "de", "es", "fr"]
 
-statsd.histogram('wiki.dd.monthly_views', last_month_views, tags=["monthly_views"])
+def get_wiki_stats(lang):
+    api_url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/" + lang + ".wikipedia.org/all-access/all-agents/Datadog/daily/20200101/" + str(today)
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(api_url, headers=hdr)
+    data = response.json()
+    return data['items'][0]['views']
+
+
+for l in languages:
+    views_today = get_wiki_stats(l)
+    statsd.histogram('wiki.dd.views_today', views_today, tags=[l])
+
+
+#views_today = data['items'][0]['views']
+#statsd.histogram('wiki.dd.views_today', views_today, tags=["monthly_views"])
