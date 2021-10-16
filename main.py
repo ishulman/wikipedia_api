26c5d5557c352a4aef554d37300881af7184b0bd
ishulman <39791045+ishulman@users.noreply.github.com>
2021-10-16 22:04:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def get_wiki_stats_l(lang):
     data = response.json()
     return data['items'][0]['views']
 
-def get_wiki_stats_a(access):
-    api_url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia.org/"+ access + "/all-agents/Datadog/monthly/" + str(yesterday) + "/" + str(yesterday)
+def get_wiki_stats_a(lang, access):
+    api_url = "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/" + lang + access + "/all-agents/Datadog/daily/" + str(yesterday) + "/" + str(yesterday)
     hdr = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(api_url, headers=hdr)
     data = response.json()
@@ -28,10 +28,19 @@ def get_wiki_stats_a(access):
 
 for l in languages:
     views_yesterday = get_wiki_stats_l(l)
-    print(views_yesterday)
-    statsd.histogram('wiki.views_yesterday', views_yesterday, tags=[l])
+    views_yesterday_desktop = get_wiki_stats_a(l, "desktop")
+    views_yesterday_mobile_app = get_wiki_stats_a(l, "mobile-app")
+    views_yesterday_mobile_web = get_wiki_stats_a(l, "mobile-web")
 
-for a in access:
-    views_yesterday_access = get_wiki_stats_a(a)
-    print(views_yesterday_access)
-    statsd.histogram('wiki.views_yesterday_access', views_yesterday_access, tags=[a])
+    statsd.histogram('wiki.views_yesterday', views_yesterday, tags=[l])
+    statsd.histogram('wiki.views_yesterday_desktop', views_yesterday_desktop, tags=[l])
+    statsd.histogram('wiki.views_yesterday_mobile_app', views_yesterday_mobile_app, tags=[l])
+    statsd.histogram('wiki.views_yesterdayy_mobile_web', views_yesterday_mobile_web, tags=[l])
+
+
+# for a in access:
+#     views_yesterday_access = get_wiki_stats_a(a)
+#     print(views_yesterday_access)
+#     statsd.histogram('wiki.views_yesterday_access', views_yesterday_access, tags=[l])
+
+
